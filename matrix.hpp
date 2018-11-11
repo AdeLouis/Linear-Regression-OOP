5e502eb78812ca2273e585ebd935e6e2240fbe0c
Louis Gomez <lagomez@shockers.wichita.edu>
2018-11-11 14:34:56 -0600

    Add private data members to matrix class

--- matrix.hpp
+++ matrix.hpp
@@ -1,4 +1,13 @@
+//Name: Louis Gomez
+//Class: Object Oriented Programming
+//Class Project
+
+#include <vector>
+//using namespace std;
+
 class Matrix
 {
-
+  unsigned int N_row;       //number of rows
+  unsigned int N_col;       //number of columns
+  vector<vector <double> > mat;   //2D vector to represent matrix
 };
