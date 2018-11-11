7fef0abbe887329f228e7cff5acb57f876f48aa9
Louis Gomez <lagomez@shockers.wichita.edu>
2018-11-11 16:07:09 -0600

    Add more member functions

--- matrix.hpp
+++ matrix.hpp
@@ -10,4 +10,21 @@ class Matrix
   unsigned int N_row;       //number of rows
   unsigned int N_col;       //number of columns
   vector<vector <double> > mat;   //2D vector to represent matrix
+
+public:
+  //constructor
+  
+  //Matrix operations
+  Matrix operator* (const Matrix& mat);      //Matrix multiplication
+  Matrix dot_multiply_matrix (const Matrix& mat, int col);
+  Matrix operator- (const Matrix& mat);      //Matrix subtraction
+  Matrix dot_squared();                  //Squares each element in the matrix
+  Matrix add_new_col (Matrix& mat);
+
+  //Scaler operations
+  double sum_matix();     //Sums the value in a matrix. Assumes N_col = 1
+
+  //other
+  unsigned int get_row() const;        //get number of rows
+  unsigned int get_column() const;    //get number of columns
 };
